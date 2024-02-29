c7af13297809a53d7d5bdbda015e530af5923244
BJ Jackson <bj.jackson.mil@swf.army.mil>
2024-02-29 16:07:26 -0600

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import {Form} from "./Components/Form.tsx";
+
+function App() {
+
+  return (
+     <div className='h-screen bg-gradient-to-r from-fuchsia-500 to-cyan-500 flex items-center justify-center'>
+       <Form/>
+     </div> )
+}
+
+export default App
